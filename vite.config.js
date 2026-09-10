ef6c7b9b3ff9d694de88c3c688721f30d941b06e
fajriainulyaqin-dev <fajriainulyaqin3@gmail.com>
2026-09-10 12:19:11 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Sales Tracker",
+        short_name: "Sales Tracker",
+        description: "Monitoring Penawaran & Pencapaian",
+        theme_color: "#080d19",
+        background_color: "#080d19",
+        display: "standalone",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
 });
